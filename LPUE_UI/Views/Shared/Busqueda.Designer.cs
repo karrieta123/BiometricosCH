148fb4c1a2f22014558504a09b872274c064678b
KarenArrietaDC <99675675+KarenArrietaDC@users.noreply.github.com>
2022-04-25 12:48:08 -0600

    busqueda

---
+++ LPUE_UI/Views/Shared/Busqueda.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace PUE.Views.Shared
+{
+    partial class Busqueda
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_Curp = new System.Windows.Forms.TextBox();
+            this.txt_Nombre = new System.Windows.Forms.TextBox();
+            this.txt_ApellidoP = new System.Windows.Forms.TextBox();
+            this.txt_ApellidoM = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_Curp
+            // 
+            this.txt_Curp.Location = new System.Drawing.Point(52, 93);
+            this.txt_Curp.Name = "txt_Curp";
+            this.txt_Curp.Size = new System.Drawing.Size(228, 20);
+            this.txt_Curp.TabIndex = 0;
+            // 
+            // txt_Nombre
+            // 
+            this.txt_Nombre.Location = new System.Drawing.Point(309, 93);
+            this.txt_Nombre.Name = "txt_Nombre";
+            this.txt_Nombre.Size = new System.Drawing.Size(228, 20);
+            this.txt_Nombre.TabIndex = 1;
+            // 
+            // txt_ApellidoP
+            // 
+            this.txt_ApellidoP.Location = new System.Drawing.Point(562, 93);
+            this.txt_ApellidoP.Name = "txt_ApellidoP";
+            this.txt_ApellidoP.Size = new System.Drawing.Size(228, 20);
+            this.txt_ApellidoP.TabIndex = 2;
+            // 
+            // txt_ApellidoM
+            // 
+            this.txt_ApellidoM.Location = new System.Drawing.Point(819, 93);
+            this.txt_ApellidoM.Name = "txt_ApellidoM";
+            this.txt_ApellidoM.Size = new System.Drawing.Size(228, 20);
+            this.txt_ApellidoM.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Curp";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(306, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Nombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(559, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Primer apellido";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(816, 74);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Segundo apellido";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(55, 141);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 39);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Buscar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Busqueda
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1153, 646);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_ApellidoM);
+            this.Controls.Add(this.txt_ApellidoP);
+            this.Controls.Add(this.txt_Nombre);
+            this.Controls.Add(this.txt_Curp);
+            this.Name = "Busqueda";
+            this.Text = "Busqueda";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_Curp;
+        private System.Windows.Forms.TextBox txt_Nombre;
+        private System.Windows.Forms.TextBox txt_ApellidoP;
+        private System.Windows.Forms.TextBox txt_ApellidoM;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ LPUE_UI/Views/Shared/Busqueda.cs
@@ -0,0 +1,34 @@
+﻿using DataPUE.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PUE.Views.Shared
+{
+    public partial class Busqueda : Form
+    {
+        EntitieLocal _context;
+
+        public Busqueda()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+            using (_context = new EntitieLocal())
+            {
+                var cIUDADANO = _context.CIUDADANO.Where(x => x.CURP == txt_Curp.Text).FirstOrDefault();
+
+                var y = cIUDADANO;
+            }
+        }
+    }
+}
